ec7fecf2daa4624db26aa259461e56834bf4df6d
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-01-28 17:16:03 +0100

    Sets up Project

---
+++ src/checkpoint/andela/log/LogWriter.java
@@ -0,0 +1,7 @@
+package checkpoint.andela.log;
+
+/**
+ * Created by Spykins on 28/01/2016.
+ */
+public class LogWriter {
+}
---
+++ src/checkpoint/andela/parser/FileParser.java
@@ -0,0 +1,7 @@
+package checkpoint.andela.parser;
+
+/**
+ * Created by Spykins on 28/01/2016.
+ */
+public class FileParser {
+}
