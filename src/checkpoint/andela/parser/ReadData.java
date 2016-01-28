69f11348a15e56a1cd9af567eef8b37c2d3efc78
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-01-28 18:30:34 +0100

    Implements ReadData Class

--- src/checkpoint/andela/parser/ReadData.java
+++ src/checkpoint/andela/parser/ReadData.java
@@ -25,8 +25,45 @@ public class ReadData {
   public HashMap<String,String> getData(){
     HashMap<String, String> newDataToAdd = new HashMap<String, String>();
 
+    while(true) {
+      try {
+        lineFromFile = bufferedReader.readLine();
+        if (lineFromFile == null || isTheEndOfData()) {
+          break;
+        }
+        //checking for comment lin that starts with /
+        if (getSplittedString(lineFromFile).length == 2) {
+          if (newDataToAdd.containsKey(getSplittedString(lineFromFile)[0])) {
+            String newValue = newDataToAdd.get(getSplittedString(lineFromFile)[0]) + "::" + getSplittedString(lineFromFile)[1];
+            newDataToAdd.put(getSplittedString(lineFromFile)[0], newValue);
 
+          } else {
+            newDataToAdd.put(getSplittedString(lineFromFile)[0], getSplittedString(lineFromFile)[1]);
+          }
+        } else {
+          if (!isTheEndOfData() && newDataToAdd.containsKey("COMMENT-INTERNAL")) {
+            String oldValue = newDataToAdd.get("COMMENT-INTERNAL");
+            newDataToAdd.put("COMMENT-INTERNAL", oldValue + lineFromFile);
+          }
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
     return  newDataToAdd;
+  }
+
+
+  private String[] getSplittedString(String presentLineRead){
+    return presentLineRead.split(" - ", 2);
+  }
+
+  private boolean isTheBeginingOfData() {
+    return lineFromFile.charAt(0) != '#';
+  }
+
+  private boolean isTheEndOfData(){
+    return lineFromFile.equals("//");
   }
 
 
